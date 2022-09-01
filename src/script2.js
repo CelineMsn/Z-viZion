46cce2235953f82905d4ea06ec46e13a33853972
CelineMsn <92699111+CelineMsn@users.noreply.github.com>
2022-09-01 00:11:34 -0700

    card adaptation portfolio

---
+++ src/script2.js
@@ -0,0 +1,19 @@
+function alertMessage(event) {
+  event.preventDefault();
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email-address").value;
+  let contactMessage = document.querySelector("#contact-message").value;
+  document.querySelector("#hero-contact").innerHTML = "";
+  let message = document.querySelector("#contact-title");
+  message.classList.add("margin");
+  document.querySelector(
+    "#contact-title"
+  ).innerHTML = `Thanks, ${name}! 😃 I'll be in touch with you shortly`;
+  sendMail(name, email, contactMessage);
+}
+
+document.querySelector("#send-mail").addEventListener("submit", alertMessage);
+
+function sendMail(event) {
+  mailTo;
+}
