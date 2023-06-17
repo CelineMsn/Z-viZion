769bacf0b92a37c54c268051ba87043024b64b07
CelineMsn <92699111+CelineMsn@users.noreply.github.com>
2023-06-17 15:14:52 -0400

    added automatic age

--- src/script.js
+++ src/script.js
@@ -4,3 +4,16 @@ for (let i = 0; i < cards.length; i++) {
     cards[i].classList.toggle("is-flipped");
   });
 }
+
+function makeAge() {
+  var age = document.querySelector("#age");
+  const ageNumber =
+    new Date().getFullYear() - new Date(1988, 10 - 1, 10).getFullYear() - 1;
+  console.log(ageNumber);
+  console.log(age)
+  //make it work later:
+  // age && (if it doesn't work or is null then it won't continue)
+  if (age)
+    age.innerHTML = ageNumber;
+}
+makeAge();
