fa308816e3f42723a23ec87ff2df945d6bd0368b
CelineMsn <92699111+CelineMsn@users.noreply.github.com>
2023-10-25 14:59:42 -0400

    fixed age js

--- src/script.js
+++ src/script.js
@@ -8,12 +8,11 @@ for (let i = 0; i < cards.length; i++) {
 function makeAge() {
   var age = document.querySelector("#age");
   const ageNumber =
-    new Date().getFullYear() - new Date(1988, 10 - 1, 10).getFullYear() - 1;
+    new Date().getFullYear() - new Date(1988, 10 - 1, 10).getFullYear();
   console.log(ageNumber);
-  console.log(age)
+  console.log(age);
   //make it work later:
   // age && (if it doesn't work or is null then it won't continue)
-  if (age)
-    age.innerHTML = ageNumber;
+  if (age) age.innerHTML = ageNumber;
 }
 makeAge();
